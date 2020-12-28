12c775036e04f1e4bf058170489843a7892ff090
Raymond Kim <raymond.kim243@outlook.com>
2020-12-28 17:51:30 -0500

    Fix a name

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello world!');
+app.get('/', (request, response) => {
+    response.status(200).send('Hello world!');
 });
 
 app.get('/data', (req, res) => {
